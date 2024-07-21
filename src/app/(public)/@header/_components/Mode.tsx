5917ac1a356be2379c016a72913a9d2ed6a76567
Osi ic <osiic.offcl@gmail.com>
2024-07-21 23:50:14 +0700

    fix2

--- src/app/(public)/@header/_components/Mode.tsx
+++ src/app/(public)/@header/_components/Mode.tsx
@@ -6,7 +6,9 @@ import { Button } from "@/components/ui/button";
 
 export function Mode() {
   const [dark, setDark] = useState<boolean>();
+
   useEffect(() => {
+    localStorage.setItem("dark", dark);
     const theme = localStorage.getItem("dark");
     setDark(theme);
   }, []);
